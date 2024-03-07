7378e006281e6c4574fa6a2d9b655498bed57c70
solisoma <solisomamadubueze@gmail.com>
2024-03-07 05:31:58 +0100

    modified main

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
     import Checkout from "./lib/Checkout.svelte";
-    import Scaffold from "./lib/Scaffold.svelte";
+    import Nav from "./lib/Nav.svelte";
 </script>
 
 <main>
     <div class="h-[7vh]">
-        <Scaffold />
+        <Nav />
     </div>
     <div class="h-[91vh] flex justify-center overflow-y-scroll remove-scrollbar border-t-2 pt-2 border-gray-300">
         <Checkout />
